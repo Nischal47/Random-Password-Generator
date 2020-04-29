c9df9be510d17ebac73318f098b3b828a4ad144c
Nischal47 <nischalbohara77@gmail.com>
2020-04-29 12:22:10 +0545

    loop to 10000 is removed

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,6 @@
 import java.security.SecureRandom
 
 fun main(){
-    for(i in 0..10000){
         val passwordGeneration = PasswordGeneration()
         passwordGeneration.generatePassword()
-    }
 }
